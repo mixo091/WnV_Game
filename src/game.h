3c1ccab78150ea2815c697d60445ddbca2e209e5
mixo091 <mmichopoulos1.1@gmail.com>
2022-11-29 12:28:06 +0200

    plannig Game structure

---
+++ src/game.h
@@ -0,0 +1,62 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include "./grid.h"
+
+//Edw ola ta headers. grid beings etc.
+
+
+
+class Game {
+
+    public:
+
+    // vector <Vampire> vampires;
+    // vector <WereWolves> werewolves
+    // Avatar avatar
+    //Grid* map;
+
+    // stat srtucture { number of w , number of v ,  number potion of a}
+
+    // bool day or night
+
+    /*
+    
+        initializeGame(){
+
+            1- Create Map.
+            2- Set beings
+
+
+
+        }
+
+
+        gameEngine()){
+
+            loop{
+
+                1- set day or night based on a cond
+                2- move being randomly in the map
+                3- fights and healing ( player based on night day cond)
+                4- check for deaths remove corpses
+                5- update stat structure.
+                6- ckeck game's ending condition to break.
+
+
+            }
+
+        }
+    
+    
+    
+    
+    */
+
+
+
+
+
+
+
+};
